713d1c0f56fa49759715bd3f87d8e64ca263e2a8
Luca Santarella <luca.santarella@gmail.com>
2017-04-26 11:43:57 -0400

    Basic Phalcon project structure

---
+++ oratorysignout/app/modules/cli/tasks/VersionTask.php
@@ -0,0 +1,13 @@
+<?php
+namespace Oratorysignout\Modules\Cli\Tasks;
+
+class VersionTask extends \Phalcon\Cli\Task
+{
+    public function mainAction()
+    {
+        $config = $this->getDI()->get('config');
+
+        echo $config['version'];
+    }
+
+}
---
+++ oratorysignout/app/modules/frontend/controllers/ControllerBase.php
@@ -0,0 +1,9 @@
+<?php
+namespace Oratorysignout\Modules\Frontend\Controllers;
+
+use Phalcon\Mvc\Controller;
+
+class ControllerBase extends Controller
+{
+
+}
