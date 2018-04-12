ae661f9504fc85c3ad17c810de3f4dfced3ce409
Luca Santarella <luca.santarella@gmail.com>
2018-04-12 09:50:34 -0400

    Cleaned up imports

--- app/modules/api/controllers/AuthRequiredControllerBase.php
+++ app/modules/api/controllers/AuthRequiredControllerBase.php
@@ -4,18 +4,6 @@ namespace Oratorysignout\Modules\Api\Controllers;
 
 use DateTime;
 use Google_Client;
-use GuzzleHttp\Psr7\Response;
-use GuzzleHttp\Psr7\ServerRequest;
-use GuzzleHttp\Psr7\Stream;
-use Lcobucci\JWT\Parser;
-use Lcobucci\JWT\Signer\Rsa\Sha256;
-use Lcobucci\JWT\Token;
-use League\OAuth2\Server\Exception\OAuthServerException;
-use Orthotrax\Models\Company;
-use Orthotrax\Models\OauthAccessTokens;
-use Orthotrax\Models\OauthClients;
-use Orthotrax\Models\Users;
-use Phalcon\Logger;
 
 
 class AuthRequiredControllerBase extends ControllerBase
