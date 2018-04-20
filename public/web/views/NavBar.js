89d53ab8ec27c16f20718cf7558fedc07ca1dab8
Luca Santarella <luca.santarella@gmail.com>
2018-04-20 12:01:48 -0400

    Hid buttons

--- public/web/views/NavBar.js
+++ public/web/views/NavBar.js
@@ -17,18 +17,14 @@ define([
       '    <a href="#name"><span class="white-text name"><%= guser.given_name %> <%= guser.family_name %></span></a>' +
       '    <a href="#email"><span class="white-text email"><%= guser.email %></span></a>' +
       '  </div></li>' +
-      '  <li><a href="sass.html">Sass</a></li>' +
-      '  <li><a href="badges.html">Components</a></li>' +
-      '  <li><a href="collapsible.html">JavaScript</a></li>' +
+      '    <li><a href="#">Temp</a></li>' +
       '</ul>' +
       '<nav class="oratory-blue darken-4 white">' +
       '<div class="nav-wrapper">' +
       '  <a href="#" class="brand-logo" style="height: 100%;"><img src="./img/logo_white.svg" width="100px" style="max-height: 100%; width: 150px;"/></a>' +
       '  <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>\n' +
       '  <ul id="nav-mobile" class="right hide-on-med-and-down">' +
-      '    <li><a href="sass.html">Sass</a></li>' +
-      '    <li><a href="badges.html">Components</a></li>' +
-      '    <li><a href="collapsible.html">JavaScript</a></li>' +
+      '    <li><a href="#">Temp</a></li>' +
       '  </ul>' +
       '</div>' +
       '</nav>' +
