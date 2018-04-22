b4ca40cd5729e88e8c4267cedc6f2b28325d63f2
Luca Santarella <luca.santarella@gmail.com>
2018-04-22 11:27:23 -0400

    Updated navbar links
    Former-commit-id: c8ca9f43e35b9b97abd801ce1d408ec94b152809

--- public/web/views/NavBar.js
+++ public/web/views/NavBar.js
@@ -2,8 +2,9 @@
 
 define([
   'underscore',
+  'backbone',
   'marionette'
-], function (_, Marionette) {
+], function (_, Backbone, Marionette) {
   return Marionette.View.extend({
 
     tagName: 'header',
@@ -17,15 +18,13 @@ define([
       '    <a href="#name"><span class="white-text name"><%= guser.given_name %> <%= guser.family_name %></span></a>' +
       '    <a href="#email"><span class="white-text email"><%= guser.email %></span></a>' +
       '  </div></li>' +
-      '    <li><a href="#">Temp</a></li>' +
+      '  <span id="nav-list-mobile-container"></span>' +
       '</ul>' +
       '<nav class="oratory-blue darken-4 white">' +
       '<div class="nav-wrapper">' +
       '  <a href="#" class="brand-logo" style="height: 100%;"><img src="./img/logo_white.svg" width="100px" style="max-height: 100%; width: 150px;"/></a>' +
       '  <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>\n' +
-      '  <ul id="nav-mobile" class="right hide-on-med-and-down">' +
-      '    <li><a href="#">Temp</a></li>' +
-      '  </ul>' +
+      '  <ul id="nav-mobile" class="right hide-on-med-and-down"></ul>' +
       '</div>' +
       '</nav>' +
       ''),
@@ -33,25 +32,50 @@ define([
     initialize: function (options) {
       this.app = options.app;
       this.model = options.model;
+      this.navLinks = new Backbone.Collection([
+        {
+          href: 'logout',
+          title: 'Logout'
+        }
+      ])
     },
 
     onRender: function () {
       M.Sidenav.init(this.$el.find('.sidenav'));
+      let childView = this.childView;
+
+      this.showChildView('sideBarLinks', new (Marionette.CollectionView.extend({
+        tagName: 'span',
+        id: 'nav-list-mobile-container',
+        childView: childView
+      }))({collection: this.navLinks}));
+
+      this.showChildView('navBarLinks', new (Marionette.CollectionView.extend({
+        tagName: 'ul',
+        id: 'nav-mobile',
+        className: 'right hide-on-med-and-down',
+        childView: childView
+      }))({collection: this.navLinks}));
     },
+
+    childView: Marionette.View.extend({
+      tagName: 'li',
+      template: _.template('<a href="#<%= href %>"><%= title %></a>'),
+    }),
 
     regions: {
 
       sideBarLinks: {
-        el: '#slide-out',
+        el: '#nav-list-mobile-container',
         replaceElement: true
       },
 
       navBarLinks: {
-        el: 'header',
+        el: '#nav-mobile',
         replaceElement: true
       }
 
-    },
+    }
 
   });
 });
